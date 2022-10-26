3995481e895ab3f4d1ca3a5d0bb834d4c8354ff5
Omar Morad <56582432+omarmorad@users.noreply.github.com>
2022-10-26 16:25:55 +0200

    Add files via upload

---
+++ data_gen.py
@@ -0,0 +1,80 @@
+
+import csv
+import random
+import time
+
+x_value = 0
+total_1 = 1000
+total_2 = 1000
+
+fieldnames = ["x_value", "total_1", "total_2"]
+
+
+with open('data_gen.csv', 'w') as csv_file:
+    csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+    csv_writer.writeheader()
+
+while True:
+
+    with open('data_gen.csv', 'a') as csv_file:
+        csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+
+        info = {
+            "x_value": x_value,
+            "total_1": total_1,
+            "total_2": total_2
+        }
+
+        csv_writer.writerow(info)
+        print(x_value, total_1, total_2)
+
+        x_value += 1
+        total_1 = total_1 + random.randint(-6, 8)
+        total_2 = total_2 + random.randint(-5, 6)
+
+    time.sleep(1)
+
+
+
+
+
+import random
+from itertools import count
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+
+plt.style.use('fivethirtyeight')
+
+x_vals = []
+y_vals = []
+
+index = count()
+
+
+def animate(i):
+    data = pd.read_csv('data_gen.csv')
+    x = data['x_value']
+    y1 = data['total_1']
+    y2 = data['total_2']
+
+    plt.cla()
+
+    plt.plot(x, y1, label='Channel 1')
+    plt.plot(x, y2, label='Channel 2')
+
+    plt.legend(loc='upper left')
+    plt.tight_layout()
+
+
+ani = FuncAnimation(plt.gcf(), animate, interval=1000)
+
+plt.tight_layout()
+plt.show()
+
+
+
+
+
+
+
